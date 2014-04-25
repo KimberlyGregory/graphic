c7d533d4c1ecf9068977770b94e4fa804bd29689
Eric Candino <ecandino@gmail.com>
2014-04-24 23:08:22 -0400

    starting to get the table graphic working

--- app/js/index.js
+++ app/js/index.js
@@ -1,19 +1,37 @@
 var Graphic = (function($, d3, undefined){
 
   var days = ['day1', 'day2', 'day3'],
-      data,
 
 
 
   init = function () {
-    window.data = getData();
-    graph();
+    var data = getData();
+    var dates = generateDates();
+    graph(dates, data);
   },
 
 
-  graph = function(){
-    d3.select('#dailyViz')
-      .style('background-color','#eee');
+  generateDates = function(){
+    var d, dates = [];
+    // var format = d3.time.format("%m-%d");
+    for (var i = 20 - 1; i >= 0; i--) {
+      d = new Date();
+
+      dates.push(d.setDate(d.getDate() - i));
+    };
+    console.log(dates);
+    return dates;
+  },
+
+  graph = function(dates){
+    d3.select('#dailyViz').append('tr')
+      .selectAll('tr')
+        .data(dates)
+      .enter().append('td')
+        .text(function(d){ return d3.time.format("%m-%d")(new Date(d)); }).
+        attr('class', 'mark');
+
+
   },
 
   getData = function(){
