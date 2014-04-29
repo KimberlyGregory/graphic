a9e98af0ce06b7c19d99ff3854c92ddf1b1e0ec6
Eric Candino <ecandino@gmail.com>
2014-04-28 21:45:53 -0400

    starting to get an understanding of d3 inner workings, svg table is in place'

--- app/js/index.js
+++ app/js/index.js
@@ -24,14 +24,16 @@ var Graphic = (function($, d3, undefined){
   },
 
   graph = function(dates){
-    d3.select('#dailyViz').append('tr')
-      .selectAll('tr')
-        .data(dates)
-      .enter().append('td')
-        .text(function(d){ return d3.time.format("%m-%d")(new Date(d)); }).
-        attr('class', 'mark');
-
-
+    d3.select('#dailyViz').append('svg').attr("width", 1000).attr("height", 200)
+    .selectAll('g')
+    .data(d3.range(0,90)).enter()
+      .append('rect')
+      .attr('fill', '#eee')
+      .attr('width', 20)
+      .attr('height', 20)
+      .attr('x', function(d, i){ return (i % 40) * 20 + 20 })
+      .attr('y', function (d, i) { return Math.floor(i / 40) * 20})
+      .attr('stroke', '#222').text( function(d, i){ return i; });
   },
 
   getData = function(){
