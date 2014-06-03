caa9e282a2585d471ae4b94512a3310a2af23166
Eric Candino <ecandino@gmail.com>
2014-06-02 22:31:29 -0400

    its working, its actually working

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ Graphic = (function ($, d3, undefined) {
 
 
 	drawGraph = function(data){
-		var width = 1000,
+		var width = 900,
     		barHeight = 20;
 
     	var x = d3.scale.linear()
@@ -14,14 +14,26 @@ Graphic = (function ($, d3, undefined) {
 						if (d.data === 'No entries'){ return 0; }
 						return d3.sum(d.data.map(function(e){ return e.valueInMinutes; }));
 					})])
-    			.range([0,width]);
+    			.range([0,width - 150]);
 
-			var color = d3.scale.ordinal()
-			    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+		var color = d3.scale.ordinal()
+			.domain(categories)
+		    .range(["#556270", "#4ECDC4", "#C7F464", "#FF6B6B", "#C44D58", "#d0743c", "#ff8c00"]);
 
     	var chart = d3.select("#bar-chart")
     	    .attr("width", width)
     	    .attr("height", barHeight * data.length);
+
+    	data.forEach(function(date){
+    		if (date.data.length < 1) { return; }
+
+    		// Iterate throught each day adding the total minutes to each entry
+    		var baseMin = 0;
+    		date.data.forEach(function(entry){
+    			entry.xVal = baseMin += entry.valueInMinutes;
+    		});
+
+    	});
 
     	var bar = chart.selectAll("g")
     	    .data(data)
@@ -31,19 +43,12 @@ Graphic = (function ($, d3, undefined) {
 			bar.selectAll("rect")
       	.data(function(d) { return d.data; })
     	.enter().append("rect")
-				.attr('x', function(d, index){
-					if (index === 0) { barStart = 0;}
-					d.start = barStart;
-					// console.log(d.start, barStart);
-					return d.start;
-				})
-				.attr('width', function(d, index, anything, uh){
-					console.log(anything);
-					d.start = d.valueInMinutes;
-					return d.valueInMinutes;
+				.attr('x', function(d){ return x(d.xVal - d.valueInMinutes); })
+				.attr('width', function(d){
+					return x(d.valueInMinutes);
 				})
 				.attr('height', barHeight - 1)
-				.style('fill', function(d, index){ return color(index); });
+				.style('fill', function(d, index){ return color(d.subject); });
 
 			bar.append("text")
     	      .attr("x", 5)
@@ -58,6 +63,25 @@ Graphic = (function ($, d3, undefined) {
 							}
 							return time + " min on "  + d.date;
 						});
+
+    	var legend = chart.selectAll(".legend")
+    	      .data(color.domain().slice().reverse())
+    	    .enter().append("g")
+    	      .attr("class", "legend")
+    	      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    	  legend.append("rect")
+    	      .attr("x", width - 18)
+    	      .attr("width", 18)
+    	      .attr("height", 18)
+    	      .style("fill", color);
+
+    	  legend.append("text")
+    	      .attr("x", width - 24)
+    	      .attr("y", 9)
+    	      .attr("dy", ".35em")
+    	      .style("text-anchor", "end")
+    	      .text(function(d) { return d; });
 	},
 
 	init = function(){
